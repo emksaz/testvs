3fa5d4ee3cae4caca9625ed78978024108e152e8
emksaz <davidz@survey.best>
2015-07-08 16:10:38 +0800

    test-v1.1.

---
+++ Testvs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testvs.Startup))]
+namespace Testvs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ gruntfile.js
@@ -0,0 +1,6 @@
+module.exports = function (grunt) {
+
+    grunt.file.defaultEncoding = 'utf8';
+    grunt.file.preserveBOM = false;
+
+};
